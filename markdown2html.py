78451ece5b2cc3c1070c3112efeed130cd3ad077
attahchisom6 <attahchisom@gmail.com>
2024-01-14 06:03:00 +0100

    add

--- markdown2html.py
+++ markdown2html.py
@@ -1,4 +1,5 @@
 #!/bin/python3
+import ast
 import os
 import sys
 import re
@@ -12,7 +13,7 @@ def parse_heading(line):
         text_heading = heading_match.group(2).strip()
         return '<h{}>{}</h{}>\n'.format(level_heading, text_heading, level_heading)
 
-    return line
+    return ""
 
 
 def syntax_line(line, symbol, tag):
@@ -30,31 +31,46 @@ def syntax_line(line, symbol, tag):
         return html
     return None
 
-def parse_paragraph(lines):
+def parse_paragraph(line):
     """
     parse desired content to html paragrapgh
     """
-    in_paragraph = False
+    """in_paragraph = False
     parsed_lines = []
 
-    for line in lines:
-        if syntax_line(line, "* ", "li") is None:
-            if not line.strip(): # evaluate to true if line is empty
-                if in_paragraph:
-                    parsed_lines.append("\n\t<br />\n</p>\n")
-                    in_paragraph = False
-                else:
-                    parsed_lines.append("\n")
-            else: # non empty line
-                if not in_paragraph:
-                    parsed_lines.append("<p>\n\t{}".format(line.strip()))
-                    in_paragraph = True
-                else:
-                    parsed_lines.append("\n\t{}".format(line.strip()))
-
+    if not line.strip(): # evaluate to true if line is empty
         if in_paragraph:
-            parsed_lines.append("\n\t<br>\n<\p>\n")
-        return "".join(parsed_lines)
+            parsed_lines.append("\n\t<br />\n</p>\n")
+            in_paragraph = False
+        else:
+            parsed_lines.append("\n")
+    else: # non empty line
+        if not in_paragraph:
+            parsed_lines.append("<p>\n\t{}".format(line.strip()))
+            in_paragraph = True
+        else:
+            parsed_lines.append("\n\t{}".format(line.strip()))
+
+    if in_paragraph:
+        parsed_lines.append("\n\t<br>\n<\p>\n")
+    return "".join(parsed_lines)"""
+    filtered = []
+    paragraph_lines = []
+    if parse_heading(line) == "" and syntax_line(line, "* ", "li") == syntax_line(line, "- ", "li") == False:
+        filtered.append(line)
+        k = 0
+        while (k < len(filtered)):
+            if filtered[k].strip() and not filtered[k - 1] and not filtered[k + 1]:
+                paragraph_lines.append(" ")
+                paragraph_lines.append(filtered[k].strip())
+                paragraph_lines.append(" ")
+            elif not filtered[k - 1] and filtered[k].strip() and filtered[k + 1].strip():
+                paragraph_lines.append(filtered[k].strip())
+                paragraph_lines.append(filtered[k + 1].strip())
+            k += 1
+        return "".join(paragraph_lines)
+    return None
+
 
 
 def markdown(filename=None, file_output=None):
@@ -75,20 +91,30 @@ def markdown(filename=None, file_output=None):
     with open(file_output, "w", encoding="utf-8") as f_out:
         dash_array = []
         asterisk_array = []
+        paragraph_array = []
         for line in lines:
             d_line = syntax_line(line, "- ", "li")
             ast_line = syntax_line(line, "* ", "li")
+            heading = parse_heading(line)
+            p_line = parse_paragraph(line)
             if d_line:
                 dash_array.append(d_line)
             elif ast_line:
                 asterisk_array.append(ast_line)
+            elif p_line:
+                paragraph_array.append(p_line)
             else:
-                f_out.write(parse_heading(line))
+                f_out.write(heading)
         if dash_array:
             f_out.write("<ul>\n{}\n</ul>\n".format("".join(dash_array)))
         if asterisk_array:
             f_out.write("<ol>\n{}\n</ol>\n".format("".join(asterisk_array)))
-        f_out.write(parse_paragraph(lines))
+        if paragraph_array:
+            for k in range(len(paragraph_array)):
+                if paragraph_array[k - 1] and paragraph_array[k + 1] == " ":
+                    f_out.write("<p>{}</p>".format(paragraph_array[k]))
+                else:
+                    f_out("<p>\n\t{}\n\t{}\n</p>\n".format(paragraph_array[k], paragraph_array[k + 1]))
 
 
 if __name__ == "__main__":
